b436c414a921499cd0b79edf86f25560fa4c70c3
José Aguero <jaguero@moldeable.com>
2020-07-16 03:53:36 -0400

    Archivo config y rutas basicas

--- server/server.js
+++ server/server.js
@@ -1,12 +1,46 @@
+require('./config/config');
+
 const express = require('express');
 const app = express();
 
-let port = process.env.port || 3000;
+const bodyParser = require('body-parser');
 
-app.get('/', (req, res) => {
-    res.json('Hola mundo');
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.json())
+
+app.get('/usuario', (req, res) => {
+    res.json('get Usuarios');
 });
 
-app.listen(port, () => {
-    console.log(`Escuchando puerto ${port}`);
+app.post('/usuario', (req, res) => {
+
+    let body = req.body;
+
+    if (body.nombre === undefined) {
+        res.status(400).json({
+            ok: false,
+            mensaje: 'El nombre es necesario'
+        });
+    } else {
+        res.json({
+            body
+        });
+    }
+});
+
+app.put('/usuario/:id', (req, res) => {
+
+    let id = req.params.id;
+
+    res.json({
+        id
+    });
+});
+
+app.delete('/usuario', (req, res) => {
+    res.json('delete Usuarios');
+});
+
+app.listen(process.env.PORT, () => {
+    console.log(`Escuchando puerto ${process.env.PORT}`);
 });
