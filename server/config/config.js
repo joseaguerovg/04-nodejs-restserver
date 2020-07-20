346fe9067f05cf71e07891c91f4357efa01bf12b
José Aguero <jaguero@moldeable.com>
2020-07-20 19:44:44 -0400

    Variable de entorno para la url de la conección a mongo

--- server/config/config.js
+++ server/config/config.js
@@ -7,7 +7,7 @@ let urlDB;
 if (process.env.NODE_ENV === 'dev') {
     urlDB = 'mongodb://localhost:27017/cafe';
 } else {
-    urlDB = 'mongodb+srv://noderest:jose1212@cluster0.bi86j.mongodb.net/cafe';
+    urlDB = process.env.MONGO_URI;
 }
 
 process.env.URL_DB = urlDB;
