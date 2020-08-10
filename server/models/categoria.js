8bc6bb209ebde8c17a1da4f00d2ca4e9dfdc3d9d
José Aguero <jaguero@moldeable.com>
2020-08-09 22:34:39 -0400

    Creación modelo categorias

---
+++ server/models/categoria.js
@@ -0,0 +1,17 @@
+const mongoose = require('mongoose');
+
+let Schema = mongoose.Schema;
+
+let categoriaSchema = new Schema({
+    nombre: {
+        type: String,
+        unique: true,
+        required: [true, 'El nombre es requerido']
+    },
+    usuario: {
+        type: Schema.Types.ObjectId,
+        ref: 'Usuario'
+    }
+});
+
+module.exports = mongoose.model('Categoria', categoriaSchema);
