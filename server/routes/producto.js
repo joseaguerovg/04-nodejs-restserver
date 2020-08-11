52e9d8b41a3119253c8470700465a3e1d83becfe
José Aguero <jaguero@moldeable.com>
2020-08-10 23:16:25 -0400

    Metodos productos

--- server/routes/producto.js
+++ server/routes/producto.js
@@ -8,9 +8,54 @@ let Producto = require('../models/producto');
 app.get('/productos', (req, res) => {
     // populate: categoria, usuario
     // paginado
+
+    let limite = Number(req.query.limite) || 5;
+    let page = Math.max(0, req.query.page) || 0;
+
+    Producto.find({ disponible: true })
+        .skip(limite * page)
+        .limit(limite)
+        .populate('categoria')
+        .populate('usuario')
+        .exec((err, productosDB) => {
+            if (err) {
+                return res.status(400).json({
+                    ok: false,
+                    err
+                });
+            }
+
+            Producto.countDocuments({ disponible: true }, (err, count) => {
+                res.json({
+                    ok: true,
+                    total: count,
+                    page: page,
+                    numPages: Math.round(count / limite),
+                    productos: productosDB
+                });
+            });
+
+        });
+
 });
 
 app.get('/productos/:id', (req, res) => {
+    let id = req.params.id;
+
+    Producto.findById(id, (err, productoDB) => {
+        if (err) {
+            return res.status(400).json({
+                ok: false,
+                err
+            });
+        }
+
+        res.json({
+            ok: true,
+            producto: productoDB
+        });
+
+    });
 
 });
 
@@ -47,10 +92,82 @@ app.post('/productos', verifyToken, (req, res) => {
 // Editar
 app.put('/productos/:id', (req, res) => {
 
+    let id = req.params.id;
+    let body = req.body;
+
+    Producto.findById(id, (err, productoDB) => {
+        if (err) {
+            return res.status(500).json({
+                ok: false,
+                err
+            });
+        }
+
+        if (!productoDB) {
+            return res.status(400).json({
+                ok: false,
+                err: {
+                    message: 'Producto no existe'
+                }
+            });
+        }
+
+        Producto.findByIdAndUpdate(id, body, { new: true, runValidators: true }, (err, productoDB) => {
+            if (err) {
+                return res.status(500).json({
+                    ok: false,
+                    err
+                });
+            }
+
+            res.json({
+                ok: true,
+                producto: productoDB
+            });
+
+        });
+
+    });
+
 });
 
 app.delete('/productos/:id', (req, res) => {
+    let id = req.params.id;
+    let disponible = { disponible: false };
 
+    Producto.findById(id, (err, productoDB) => {
+        if (err) {
+            return res.status(500).json({
+                ok: false,
+                err
+            });
+        }
+
+        if (!productoDB) {
+            return res.status(400).json({
+                ok: false,
+                err: {
+                    message: 'Producto no existe'
+                }
+            });
+        }
+
+        Producto.findByIdAndUpdate(id, disponible, { new: true }, (err, productoDB) => {
+            if (err) {
+                return res.status(500).json({
+                    ok: false,
+                    err
+                });
+            }
+
+            res.json({
+                ok: true,
+                producto: productoDB
+            });
+
+        });
+
+    });
 });
 
 module.exports = app;
